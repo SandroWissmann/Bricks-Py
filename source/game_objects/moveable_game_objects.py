81b8332e7a957d172a57466f1045b17926169933
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-17 15:12:50 +0200

    feat: adds moveable game object abstract class to represent moveable objects

---
+++ source/game_objects/moveable_game_objects.py
@@ -0,0 +1,29 @@
+from Bricks_py.source.game_objects.game_object import GameObject
+from Bricks_py.source.types.angle import Angle
+from Bricks_py.source.types.point import Point
+
+
+class MoveableGameObject(GameObject):
+    def __init__(
+        self,
+        top_left: Point = Point(0.0, 0.0),
+        width: float = 0.0,
+        height: float = 0.0,
+        velocity: float = 0.0,
+    ):
+        if type(self) is MoveableGameObject:
+            raise Exception(
+                "MoveableGameObject is an abstract class and cannot be "
+                "instantiated directly"
+            )
+        GameObject.__init__(self, top_left, width, height)
+        self._velocity = velocity
+
+    @property
+    def velocity(self) -> float:
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, velocity: float):
+        self._velocity = velocity
+
