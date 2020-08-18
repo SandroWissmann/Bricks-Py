f7769b072073d550fd0c566f1fa17fa0b474a606
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-18 17:55:49 +0200

    test: adds test for indestructible brick

---
+++ bricks/tests/game_objects/test_indestructible_brick.py
@@ -0,0 +1,20 @@
+from bricks.source.game_objects.indestructible_brick import IndestructibleBrick
+from bricks.source.types.point import Point
+
+
+class TestIndestructibleBrick:
+    def test_init_default(self):
+        obj = IndestructibleBrick()
+
+        assert obj.top_left.x == 0.0
+        assert obj.top_left.y == 0.0
+        assert obj.width == 0.0
+        assert obj.height == 0.0
+
+    def test_init(self):
+        obj = IndestructibleBrick(Point(8.2, 1.3), 3.1, 4.2)
+
+        assert obj.top_left.x == 8.2
+        assert obj.top_left.y == 1.3
+        assert obj.width == 3.1
+        assert obj.height == 4.2
