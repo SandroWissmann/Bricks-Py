54185ddaa1fc5836c8ba70260a90bb2f6dae2ba3
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-18 17:55:31 +0200

    feat: adds class to represent indestructible brick game object

---
+++ bricks/source/game_objects/indestructible_brick.py
@@ -0,0 +1,13 @@
+from bricks.source.game_objects.game_object import GameObject
+from bricks.source.types.point import Point
+
+
+class IndestructibleBrick(GameObject):
+    def __init__(
+        self,
+        top_left: Point = Point(0.0, 0.0),
+        width: float = 0.0,
+        height: float = 0.0,
+    ):
+        GameObject.__init__(self, top_left, width, height)
+
