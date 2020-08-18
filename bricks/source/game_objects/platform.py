2eb489b0a64d41e274d8ad01cd72349034bec827
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-18 18:23:08 +0200

    feat: adds platform class to represent platform game object

---
+++ bricks/source/game_objects/platform.py
@@ -0,0 +1,21 @@
+from bricks.source.game_objects.moveable_game_objects import MoveableGameObject
+from bricks.source.types.point import Point
+
+
+class Platform(MoveableGameObject):
+    def __init__(
+        self,
+        top_left: Point = Point(0.0, 0.0),
+        width: float = 0.0,
+        height: float = 0.0,
+        velocity: float = 0.0,
+    ):
+        MoveableGameObject.__init__(self, top_left, width, height, velocity)
+
+    def move(self, elapsed_time_in_ms: float):
+        if self.velocity == 0.0:
+            return
+
+        delta_x = elapsed_time_in_ms / 1000.0 * self.velocity
+        self.top_left.x = self.top_left.x + delta_x
+
