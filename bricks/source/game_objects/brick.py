d40ac4ec9b23fc70adbe507609317ce8927e6770
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-18 17:49:51 +0200

    feat: adds class to represent brick game object

---
+++ bricks/source/game_objects/brick.py
@@ -0,0 +1,40 @@
+from bricks.source.game_objects.game_object import GameObject
+from bricks.source.types.point import Point
+
+
+class Brick(GameObject):
+    def __init__(
+        self,
+        top_left: Point = Point(0.0, 0.0),
+        width: float = 0.0,
+        height: float = 0.0,
+        hitpoints: int = 1,
+    ):
+        GameObject.__init__(self, top_left, width, height)
+        self._start_hitpoints = _check_args_hitpoints(hitpoints)
+        self._hitpoints = self._start_hitpoints
+
+    @property
+    def start_hitpoints(self) -> int:
+        return self._start_hitpoints
+
+    @property
+    def hitpoints(self) -> int:
+        return self._hitpoints
+
+    def decrease_hitpoints(self):
+        if self.hitpoints > 0:
+            self._hitpoints -= 1
+
+    def is_destroyed(self) -> bool:
+        return self.hitpoints == 0
+
+
+def _check_args_hitpoints(hitpoints: int) -> int:
+    if hitpoints < 1 or hitpoints > 9:
+        raise ValueError(
+            "brick.def _check_args_hitpoints(hitpoints):\n"
+            "hitpoints must be >= 0 and < 10\n"
+            "hitpoints: %s\n" % (hitpoints)
+        )
+    return hitpoints
