4b98dd8b14363813623801d94e8eefcddc3a5118
SandroWissmann <sandro.wissmann@gmail.com>
2020-08-28 16:03:27 +0200

    feat: adds property values to DifficultyParameter

--- src/bricks/difficulty_parameters.py
+++ src/bricks/difficulty_parameters.py
@@ -6,7 +6,39 @@ class DifficultyParameters:
         ball_velocity: float = 16.0,
         ball_gravity: float = 1.5,
     ):
-        self.platform_velocity = platform_velocity
-        self.platform_width = platform_width
-        self.ball_velocity = ball_velocity
-        self.ball_gravity = ball_gravity
+        self._platform_velocity = platform_velocity
+        self._platform_width = platform_width
+        self._ball_velocity = ball_velocity
+        self._ball_gravity = ball_gravity
+
+    @property
+    def platform_velocity(self) -> float:
+        return self._platform_velocity
+
+    @platform_velocity.setter
+    def platform_velocity(self, platform_velocity: float):
+        self._platform_velocity = platform_velocity
+
+    @property
+    def platform_width(self) -> float:
+        return self._platform_velocity
+
+    @platform_width.setter
+    def platform_width(self, platform_width: float):
+        self._platform_width = platform_width
+
+    @property
+    def ball_velocity(self) -> float:
+        return self._platform_velocity
+
+    @ball_velocity.setter
+    def ball_velocity(self, ball_velocity: float):
+        self._ball_velocity = ball_velocity
+
+    @property
+    def ball_gravity(self) -> float:
+        return self._ball_gravity
+
+    @ball_gravity.setter
+    def ball_gravity(self, ball_gravity: float):
+        self._ball_gravity = ball_gravity
