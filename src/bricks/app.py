3cff27da9c924c79699b194a26959d39451ee952
SandroWissmann <sandro.wissmann@gmail.com>
2020-08-28 15:35:07 +0200

    fix: fixxes double declaration of constants

--- src/bricks/app.py
+++ src/bricks/app.py
@@ -5,15 +5,9 @@ SCREEN_HEIGHT = 540
 
 
 def main():
-    SCREEN_WIDTH = 780
-    SCREEN_HEIGHT = 540
-
     game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
     game.run()
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    finally:
-        quit()
+    main()
