af62ad1c48b7176fbef15267a98500920bb2f701
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-02 17:28:58 +0200

    docs: adds docstrings for brick module

--- src/bricks/game_objects/brick.py
+++ src/bricks/game_objects/brick.py
@@ -1,8 +1,38 @@
+"""Representation of a brick on the game board."""
 from bricks.game_objects.game_object import GameObject
 from bricks.types.point import Point
 
 
 class Brick(GameObject):
+    """
+    Representation of a brick on the game board.
+
+    Attributes
+    ----------
+    top_left: Point
+        Top left coordinate of the brick.
+    top_right: Point
+        Top right coordinate of the brick.
+    bottom_left: Point
+        Bottom left coordinate of the brick.
+    bottom_right: Point
+        Bottom right coordinate of the brick.
+    width: float
+        Width of the brick.
+    height: float
+        Height of the brick.
+    hitpoints: int
+        Actual hitpoints of the brick.
+    start_hitpoints: int
+        Hitpoints the brick has on creation
+
+    Methods
+    -------
+    is_destroyed(self) -> bool:
+        Indicates if brick is destroyed.
+
+    """
+
     def __init__(
         self,
         top_left: Point = Point(0.0, 0.0),
@@ -10,6 +40,9 @@ class Brick(GameObject):
         height: float = 0.0,
         hitpoints: int = 1,
     ):
+        """
+        Raises ValueError if hitpoints not in range between 1 and 9.
+        """
         GameObject.__init__(self, top_left, width, height)
         self._start_hitpoints = _check_args_hitpoints(hitpoints)
         self._hitpoints = self._start_hitpoints
@@ -27,6 +60,7 @@ class Brick(GameObject):
             self._hitpoints -= 1
 
     def is_destroyed(self) -> bool:
+        """Indicates if brick is destroyed. True if no hitpoints left"""
         return self._hitpoints == 0
 
 
