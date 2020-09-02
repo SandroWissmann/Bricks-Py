787aca69fa67cc8b4642c6626fcfa0bad5bf1665
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-02 17:29:38 +0200

    docs: adds docstrings for indestructible brick module

--- src/bricks/game_objects/indestructible_brick.py
+++ src/bricks/game_objects/indestructible_brick.py
@@ -1,8 +1,28 @@
+"""Representation of an indestructible brick on the game board."""
 from bricks.game_objects.game_object import GameObject
 from bricks.types.point import Point
 
 
 class IndestructibleBrick(GameObject):
+    """
+    Representation of an indestructible brick on the game board.
+    
+    Attributes
+    ----------
+    top_left: Point
+        Top left coordinate of the indestructible brick.
+    top_right: Point
+        Top right coordinate of the indestructible brick.
+    bottom_left: Point
+        Bottom left coordinate of the indestructible brick.
+    bottom_right: Point
+        Bottom right coordinate of the indestructible brick.
+    width: float
+        Width of the indestructible brick.
+    height: float
+        Height of the indestructible brick.
+    """
+
     def __init__(
         self,
         top_left: Point = Point(0.0, 0.0),
