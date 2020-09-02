4f631fd69655d345a88b3e0609fe546698404ad3
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-02 17:30:19 +0200

    docs: adds docstrings for wall module

--- src/bricks/game_objects/wall.py
+++ src/bricks/game_objects/wall.py
@@ -1,8 +1,28 @@
+"""Representation of a wall on the game board."""
 from bricks.game_objects.game_object import GameObject
 from bricks.types.point import Point
 
 
 class Wall(GameObject):
+    """
+    Representation of a wall on the game board.
+    
+    Attributes
+    ----------
+    top_left: Point
+        Top left coordinate of the platform.
+    top_right: Point
+        Top right coordinate of the platform.
+    bottom_left: Point
+        Bottom left coordinate of the platform.
+    bottom_right: Point
+        Bottom right coordinate of the platform.
+    width: float
+        Width of the platform.
+    height: float
+        Height of the platform.
+    """
+
     def __init__(
         self,
         top_left: Point = Point(0.0, 0.0),
