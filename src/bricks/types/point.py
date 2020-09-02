1d6652080d543af582f8b9d00c3f1b4e1017b803
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-02 15:31:28 +0200

    docs: adds docstrings for point module

--- src/bricks/types/point.py
+++ src/bricks/types/point.py
@@ -1,4 +1,19 @@
+"""Representation of a point in two dimensional coordinate system"""
+
+
 class Point:
+    """
+    Representation of a point in two dimensional coordinate system
+    
+    Attributes
+    ----------
+    x : float 
+        x component of coordinate
+    y : float 
+        y component of coordinate
+    """
+
     def __init__(self, x, y):
+        """Stores x and y"""
         self.x = x
         self.y = y
