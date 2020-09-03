605a4d24a385b61bd1776ae1a4e37d94af415a73
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-03 10:15:03 +0200

    docs: adds docstrings for audio_device module

--- src/bricks/audio_device.py
+++ src/bricks/audio_device.py
@@ -1,3 +1,21 @@
+"""
+Module to play sounds in the game. 
+
+class AudioDevice
+    Audio device needs to be constructed to play sounds.
+    Use free standing functions to play sounds.
+    Only one audio device should be created.
+
+Functions:
+    play_destroy_brick(audio_device: AudioDevice)
+    play_hit_brick(audio_device: AudioDevice)
+    play_hit_platform(audio_device: AudioDevice)
+    play_game_over(audio_device: AudioDevice)
+    play_next_level(audio_device: AudioDevice)
+    play_lost_ball(audio_device: AudioDevice)
+    play_extra_life(audio_device: AudioDevice)
+    play_win_game(audio_device: AudioDevice)
+"""
 import pygame
 
 FILENAME_DESTROY_BRICK = "sounds/destroyBrick.wav"
@@ -11,6 +29,8 @@ FILENAME_WIN_GAME = "sounds/winGame.wav"
 
 
 class AudioDevice:
+    """AudioDevice to play sounds with freestanding functions."""
+
     def __init__(self):
         pygame.mixer.init()
 
@@ -20,32 +40,40 @@ class AudioDevice:
 
 
 def play_destroy_brick(audio_device: AudioDevice):
+    """Play destroy brick sound."""
     audio_device._play_sound(FILENAME_DESTROY_BRICK)
 
 
 def play_hit_brick(audio_device: AudioDevice):
+    """Play hit brick sound."""
     audio_device._play_sound(FILENAME_HIT_BRICK)
 
 
 def play_hit_platform(audio_device: AudioDevice):
+    """Play hit platform sound."""
     audio_device._play_sound(FILENAME_HIT_PLATFORM)
 
 
 def play_game_over(audio_device: AudioDevice):
+    """Play game over sound."""
     audio_device._play_sound(FILENAME_GAME_OVER)
 
 
 def play_next_level(audio_device: AudioDevice):
+    """Play next level sound."""
     audio_device._play_sound(FILENAME_NEXT_LEVEL)
 
 
 def play_lost_ball(audio_device: AudioDevice):
+    """Play lost ball sound."""
     audio_device._play_sound(FILENAME_LOST_BALL)
 
 
 def play_extra_life(audio_device: AudioDevice):
+    """Play extra life sound."""
     audio_device._play_sound(FILENAME_EXTRA_LIFE)
 
 
 def play_win_game(audio_device: AudioDevice):
+    """Play win game sound."""
     audio_device._play_sound(FILENAME_WIN_GAME)
